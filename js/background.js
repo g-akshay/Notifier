c30645604d4546b543bd98b1535c5ac738151be2
G Akshay <gakshayapps@gmail.com>
2016-04-22 01:05:37 +0530

    added splashscreen

--- js/background.js
+++ js/background.js
@@ -1,3 +1,5 @@
+var notifierFile;
+
 chrome.app.runtime.onLaunched.addListener(function() {
     chrome.app.window.create('main.html', {
             // 'id' serves two purposes: first, it prevents you from opening
@@ -6,14 +8,14 @@ chrome.app.runtime.onLaunched.addListener(function() {
             'id': 'Notifier',
             'outerBounds': {
                 'width': 800,
-                'height': 600,
                 'minWidth': 600,
+                'height': 600,
                 'minHeight': 400
             },
             'resizable': true
         },
         function(appWindow) {
-            // callback code
+            // callback code if any
         });
 });
 
@@ -26,17 +28,25 @@ chrome.runtime.onSuspend.addListener(function() {
 });
 
 
-chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
+
+/*chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
     console.log(token);
     // add check for token existance, if not store locally and ask login again at some point
-
-    chrome.syncFileSystem.requestFileSystem(function(fs) {
-        // FileSystem API should just work on the returned 'fs'.
-        console.log(fs);
-        fs.root.getFile('notifier.txt', { create: true });
-    });
+    // apps cat be installed without login.. check again
 });
 
+chrome.syncFileSystem.requestFileSystem(function(fs) {
+    // create/get file from root
+    fs.root.getFile('notifier.txt', { create: true }, function(fileEntry) {
+        notifierFile = fileEntry;
+        //console.log(utils.writeData(notifierFile, '{"name":"akshay1"}'));
+        // console.log(utils.readData(notifierFile));
+    });
+
+    chrome.syncFileSystem.getUsageAndQuota(fs, function(data) {
+        console.log(data);
+    });
+});*/
 
 /*(function show(options) {
     var title = "Time for a Walk";
--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
     $(".button-collapse").sideNav();
+
+    setTimeout(function() {
+        $('#mainCon').show();
+        $('#splashScreen').fadeOut();
+    }, 4000);
+
+
+    /*$('#splashScreen').hide();*/
 });
 
 var m = $('.cards-con').masonry({
